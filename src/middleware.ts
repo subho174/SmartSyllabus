4fd7ffeee604efa2e19c7403b73a95f029290863
Subhodip <subhodipnebu51@gmail.com>
2025-08-14 01:40:33 +0530

    debugging for next-auth bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,7 @@ export async function middleware(req: NextRequest) {
 
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   console.log(token);
+  console.log("COOKIES:", req.cookies.getAll());
 
   if (!PUBLIC_ROUTES.includes(path) && !token)
     return NextResponse.redirect(new URL("/sign-in", req.url));
